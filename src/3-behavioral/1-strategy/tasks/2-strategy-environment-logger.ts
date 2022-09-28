17efd39f9ae03a617893e2fb2c76b99dbe7bc798
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-28 09:59:05 +0200

    feat: strategy tasks

--- src/3-behavioral/1-strategy/tasks/2-strategy-environment-logger.ts
+++ src/3-behavioral/1-strategy/tasks/2-strategy-environment-logger.ts
@@ -1,2 +1,29 @@
-const environment = process.env.NODE_ENV;
-environment === "production";
+import { ConsoleWriter, JsonFormatter, LogEntry, Logger, SimpleFormatter, TextFileWriter } from "./logger";
+
+export class LoggerStrategyFactory {
+  private static defaultStrategy = new Logger(new ConsoleWriter(), new SimpleFormatter());
+  private static productionStrategy = new Logger(new TextFileWriter(), new JsonFormatter());
+  public static chooseStrategy(): Logger {
+    // ! chosen on environment basis
+    const environment = process.env.NODE_ENV;
+    if (environment === "production") {
+      return this.productionStrategy;
+    } else {
+      return this.defaultStrategy;
+    }
+  }
+}
+
+export class Client {
+  private readonly logger = LoggerStrategyFactory.chooseStrategy();
+  public log(entry: LogEntry) {
+    this.logger.log(entry);
+  }
+}
+
+const client = new Client();
+client.log({
+  category: "info",
+  message: "Hello World",
+  timestamp: new Date(),
+});
--- src/3-behavioral/1-strategy/tasks/3-strategy-category-logger.ts
+++ src/3-behavioral/1-strategy/tasks/3-strategy-category-logger.ts
@@ -1,1 +1,45 @@
+import { ConsoleWriter, Formatter, JsonFormatter, LogEntry, SimpleFormatter, TextFileWriter, Writer } from "./logger";
+
+export type EntryStrategy = { writer: Writer; formatter: Formatter };
+
+export class EntryStrategyFactory {
+  private static defaultStrategy: EntryStrategy = { writer: new ConsoleWriter(), formatter: new SimpleFormatter() };
+  private static errorStrategy: EntryStrategy = { writer: new TextFileWriter(), formatter: new JsonFormatter() };
+
+  public static chooseStrategy(entry: LogEntry): EntryStrategy {
+    // ! chosen on parameter basis
+    // clean: replace if with a lookup table
+    if (entry.category === "error") {
+      return EntryStrategyFactory.errorStrategy;
+    } else {
+      return EntryStrategyFactory.defaultStrategy;
+    }
+  }
+}
+
 // ! redefine Logger class
+export class Logger {
+  public log(entry: LogEntry) {
+    const strategy = EntryStrategyFactory.chooseStrategy(entry);
+    strategy.writer.write(strategy.formatter.format(entry));
+  }
+}
+
+export class Client {
+  private readonly logger: Logger = new Logger();
+  public log(entry: LogEntry) {
+    this.logger.log(entry);
+  }
+}
+
+const client = new Client();
+client.log({
+  category: "info",
+  message: "Hello World",
+  timestamp: new Date(),
+});
+client.log({
+  category: "error",
+  message: "The world is ending",
+  timestamp: new Date(),
+});
