ca40952a84c5e9d97d026c0fe8c88491a078c72a
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-28 11:20:38 +0200

    feat: observer tasks

--- src/3-behavioral/2-observer/tasks/agency-subject.ts
+++ src/3-behavioral/2-observer/tasks/agency-subject.ts
@@ -1,17 +1,40 @@
-import { Agency } from "./agency";
-import { Observer } from "./logger.observer";
+import { Agency, Booking } from "./agency";
+import { Observer } from "./observer-logger";
 
 export interface Observable {
   subscribe(observer: Observer): void;
   notifyObservers(businessEvent: string, message: string): void;
 }
-
+/**
+ * An Agency wrapper that implements the Observable interface.
+ * Is a decorator that adds the subscribe and notifyObservers method to the Agency class.
+ * @param {Agency} agency
+ */
 export class AgencySubject implements Observable {
+  private observers: Observer[] = [];
   constructor(private agency: Agency) {}
-  subscribe(observer: Observer): void {
-    throw new Error("Method not implemented.");
+  public subscribe(observer: Observer): void {
+    this.observers.push(observer);
   }
-  notifyObservers(businessEvent: string, message: string): void {
-    throw new Error("Method not implemented.");
+  public notifyObservers(businessEvent: string, message: string): void {
+    this.observers.forEach(obs => obs.notify(businessEvent, message));
+  }
+  public createBooking(trip: string, price: number): Booking | undefined {
+    try {
+      const result = this.agency.createBooking(trip, price);
+      this.notifyObservers("booking-created", JSON.stringify(result));
+      return result;
+    } catch (error) {
+      this.notifyObservers("exception", (error as Error).message);
+    }
+  }
+  public cancelBooking(booking: Booking): Booking | undefined {
+    try {
+      const result = this.agency.cancelBooking(booking);
+      this.notifyObservers("booking-cancelled", JSON.stringify(result));
+      return result;
+    } catch (error) {
+      this.notifyObservers("exception", (error as Error).message);
+    }
   }
 }
--- src/3-behavioral/2-observer/tasks/logger.observer.ts
+++ src/3-behavioral/2-observer/tasks/logger.observer.ts
@@ -1,12 +1,20 @@
-import { Logger } from "./logger";
-
+import { LogEntry, Logger } from "./logger";
 export interface Observer {
   notify(businessEvent: string, message: string): void;
 }
 
+/**
+ * A Logger wrapper that implements the Observer interface.
+ * Is a decorator that adds the notify method to the Logger class.
+ */
 export class LoggerObserver implements Observer {
   private logger: Logger = new Logger();
-  notify(businessEvent: string, message: string): void {
-    throw new Error("Method not implemented.");
+  public notify(businessEvent: string, message: string) {
+    const entry: LogEntry = {
+      category: businessEvent === "exception" ? "error" : "info",
+      message: businessEvent + " - " + message,
+      timestamp: new Date(),
+    };
+    this.logger.log(entry);
   }
 }
