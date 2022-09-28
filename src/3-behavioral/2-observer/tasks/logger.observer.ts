8e757080c50c1a60d25eb09cce823be76065bff1
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-28 11:34:50 +0200

    fix: adapter

--- src/3-behavioral/2-observer/tasks/logger.observer.ts
+++ src/3-behavioral/2-observer/tasks/logger.observer.ts
@@ -1,4 +1,4 @@
-import { LogEntry, Logger } from "./logger";
+import { LogCategory, LogEntry, Logger } from "./logger";
 export interface Observer {
   notify(businessEvent: string, message: string): void;
 }
@@ -11,10 +11,14 @@ export class LoggerObserver implements Observer {
   private logger: Logger = new Logger();
   public notify(businessEvent: string, message: string) {
     const entry: LogEntry = {
-      category: businessEvent === "exception" ? "error" : "info",
+      category: this.adaptBusinessEvent(businessEvent),
       message: businessEvent + " - " + message,
       timestamp: new Date(),
     };
     this.logger.log(entry);
+  }
+
+  private adaptBusinessEvent(businessEvent: string): LogCategory {
+    return businessEvent === "exception" ? "error" : "info";
   }
 }
