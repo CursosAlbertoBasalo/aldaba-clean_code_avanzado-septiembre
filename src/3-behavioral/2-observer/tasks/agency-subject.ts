f543524b910f64c57d25ef915ae09d199fbe78cd
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-28 11:36:06 +0200

    fix: rename logger

--- src/3-behavioral/2-observer/tasks/agency-subject.ts
+++ src/3-behavioral/2-observer/tasks/agency-subject.ts
@@ -1,10 +1,11 @@
 import { Agency, Booking } from "./agency";
-import { Observer } from "./observer-logger";
+import { Observer } from "./logger.observer";
 
 export interface Observable {
   subscribe(observer: Observer): void;
   notifyObservers(businessEvent: string, message: string): void;
 }
+
 /**
  * An Agency wrapper that implements the Observable interface.
  * Is a decorator that adds the subscribe and notifyObservers method to the Agency class.
@@ -12,7 +13,9 @@ export interface Observable {
  */
 export class AgencySubject implements Observable {
   private observers: Observer[] = [];
+
   constructor(private agency: Agency) {}
+
   public subscribe(observer: Observer): void {
     this.observers.push(observer);
   }
